b1ef0f2db7183a78ca2687eb6ac7d24e1e39c070
Azat Nurulin <79209902+Sagowara@users.noreply.github.com>
2025-02-19 10:01:48 +0300

    Create index.ts

---
+++ automated-tests/src/pageObjects/triggers/index.ts
@@ -0,0 +1,6 @@
+
+export { verifyFiltersSidebar } from './filters';
+export { deleteCreatedTrigger } from './helpers';
+export { createTriggerWithoutGoal } from './createTrigger';
+export { createTriggerWithGoal } from './createTrigger';
+export { editTheTrigger } from './createTrigger';
